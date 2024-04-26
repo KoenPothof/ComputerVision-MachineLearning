4e0a58f675c37e850ec8e456f723906a3ea6574f
jewiegel <j.wiegel@student.avans.nl>
2024-04-26 12:46:29 +0200

    puter vision time

--- GroeidocumentCVML_Groep9/Practicum1/Practicum1.cpp
+++ GroeidocumentCVML_Groep9/Practicum1/Practicum1.cpp
@@ -7,6 +7,7 @@ using namespace cv;
 using namespace std;
 
 void opdracht2();
+void opdracht3();
 void opdracht4();
 
 void main() 
@@ -32,6 +33,23 @@ void opdracht2()
 	imshow("Original Image", img);
 	imshow("Image Resize", imgResize);
 	imshow("Image Crop", imgCrop);
+	waitKey(0);
+}
+
+void opdracht3() 
+{
+	Mat img(512, 512, CV_8UC3, Scalar(0, 255, 0));
+
+	circle(img, Point(256, 256), 155, Scalar(255, 0, 0), FILLED);
+
+	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), 3);
+
+	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
+
+	putText(img, "Opdracht 3", Point(137, 262), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 0, 255), 2);
+
+	imshow("Image", img);
+
 	waitKey(0);
 }
 
