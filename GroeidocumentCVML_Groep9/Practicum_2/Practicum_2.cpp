daaaa5e8de37d9c2e25aebb72c09138b6d53ff90
KoenPothof <koenpothof@gmail.com>
2024-05-13 14:22:59 +0200

    opdracht 2.4 af

--- GroeidocumentCVML_Groep9/Practicum_2/Practicum_2.cpp
+++ GroeidocumentCVML_Groep9/Practicum_2/Practicum_2.cpp
@@ -8,25 +8,65 @@ using namespace std;
 
 void opdracht1e();
 void opdracht1f();
+void opdracht4a();
+
+Mat imgHSV, mask;
+int hmin = 0, smin = 110, vmin = 153;
+int hmax = 19, smax = 130, vmax = 255;
 
 void main()
 {
-    string path = "Resources/test.png";
+    string path = "Resources/bal.png";
     Mat img = imread(path);
-    Mat imgCanny, imgDilate, imgErode;
+    Mat imgHSV;
 
-    GaussianBlur(img, img, Size(7, 7), 0, 0);
-    Canny(img, imgCanny, 50, 150);
-    Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
-    dilate(imgCanny, imgDilate, kernel);
-    erode(imgDilate, imgErode, kernel);
+    cvtColor(img, imgHSV, COLOR_BGR2HSV);
 
+    namedWindow("Trackbars", (640, 200));
+    createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+    createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+    createTrackbar("Sat Min", "Trackbars", &smin, 255);
+    createTrackbar("Sat Max", "Trackbars", &smax, 255);
+    createTrackbar("Val Min", "Trackbars", &vmin, 255);
+    createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
-    imshow("Image", img);
-    imshow("CannyImage", imgCanny);
-    imshow("DilateImage", imgDilate);
-    imshow("ErodeImage", imgErode);
-    waitKey(0);
+    while (true) {
+        Scalar lower(hmin, smin, vmin);
+        Scalar upper(hmax, smax, vmax);
+        inRange(imgHSV, lower, upper, mask);
+
+        imshow("Image", img);
+        imshow("ImageHSV", imgHSV);
+        imshow("ImageMask", mask);
+        waitKey(1);
+    }
+}
+
+void opdracht4a() {
+    string path = "Resources/bal.png";
+    Mat img = imread(path);
+    Mat imgHSV;
+
+    cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+    namedWindow("Trackbars", (640, 200));
+    createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+    createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+    createTrackbar("Sat Min", "Trackbars", &smin, 255);
+    createTrackbar("Sat Max", "Trackbars", &smax, 255);
+    createTrackbar("Val Min", "Trackbars", &vmin, 255);
+    createTrackbar("Val Max", "Trackbars", &vmax, 255);
+
+    while (true) {
+		Scalar lower(hmin, smin, vmin);
+		Scalar upper(hmax, smax, vmax);
+		inRange(imgHSV, lower, upper, mask);
+
+		imshow("Image", img);
+		imshow("ImageHSV", imgHSV);
+		imshow("ImageMask", mask);
+		waitKey(1);
+	}
 }
 
 void opdracht1e()
