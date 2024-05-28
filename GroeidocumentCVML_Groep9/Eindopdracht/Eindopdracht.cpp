59239f9550d07bf9c8eda1e1baf0f32e64a8cddb
KoenPothof <koenpothof@gmail.com>
2024-05-28 22:10:07 +0200

    kleine opzet eindopdracht

--- GroeidocumentCVML_Groep9/Eindopdracht/Eindopdracht.cpp
+++ GroeidocumentCVML_Groep9/Eindopdracht/Eindopdracht.cpp
@@ -1,20 +1,42 @@
-// Eindopdracht.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 #include <iostream>
 
-int main()
+using namespace cv;
+using namespace std;
+
+Mat img;
+VideoCapture cap(0);
+
+
+void main()
 {
-    std::cout << "Hello World!\n";
+	
+	CascadeClassifier plateCascade;
+	plateCascade.load("Resources/haarcascade_russian_plate_number.xml");
+
+	if (plateCascade.empty()) { cout << "XML file not loaded" << endl; }
+
+	vector<Rect> plates;
+	
+
+	while (true) {
+
+		cap.read(img);
+
+		plateCascade.detectMultiScale(img, plates, 1.1, 10);
+
+	for (int i = 0; i < plates.size(); i++)
+	{
+		Mat imgCrop = img(plates[i]);
+
+		imwrite("Recources/plate"+ to_string(i) + ".png", imgCrop);
+		rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
+	}
+
+	imshow("Image", img);
+	waitKey(1);
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
