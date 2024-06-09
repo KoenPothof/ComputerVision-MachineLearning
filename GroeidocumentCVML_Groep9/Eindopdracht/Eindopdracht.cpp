a394e9635a3b51c65f9d96527d3e3df50298b22d
jewiegel <j.wiegel@student.avans.nl>
2024-06-09 16:25:33 +0200

    fixed eindopdracht

--- GroeidocumentCVML_Groep9/Eindopdracht/Eindopdracht.cpp
+++ GroeidocumentCVML_Groep9/Eindopdracht/Eindopdracht.cpp
@@ -1,20 +1,76 @@
-// Eindopdracht.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace cv;
+using namespace std;
+
+void extractCharacters(Mat& plate, const string& plateName) {
+    cvtColor(plate, plate, COLOR_BGR2GRAY);
+    GaussianBlur(plate, plate, Size(7, 7), 0);
+    Canny(plate, plate, 50, 150);
+
+
+    vector<vector<Point>> contours;
+    findContours(plate, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    sort(contours.begin(), contours.end(), [](const vector<Point>& a, const vector<Point>& b) {
+        return boundingRect(a).x < boundingRect(b).x;
+        });
+
+    int count = 0;
+    for (size_t i = 0; i < contours.size(); i++) {
+        Rect bbox = boundingRect(contours[i]);
+
+        double contourArea = cv::contourArea(contours[i]);
+        if (contourArea < 100 || contourArea > 1000)
+            continue;
+
+        Mat charImg = plate(bbox);
+        string charFileName = "Resources/NumbersLetter/" + plateName + "_" + to_string(count + 1) + ".png";
+        count++;
+        imwrite(charFileName, charImg);
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    Mat img;
+    VideoCapture cap(0);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    CascadeClassifier plateCascade;
+
+    plateCascade.load("Resources/haarcascade_russian_plate_number.xml");
+
+    if (plateCascade.empty()) {
+        cout << "XML file not loaded" << endl;
+        return -1;
+    }
+
+    vector<Rect> plates;
+
+    while (true) {
+        cap.read(img);
+
+        plateCascade.detectMultiScale(img, plates, 1.1, 10);
+
+        for (int i = 0; i < plates.size(); i++) {
+            Mat plateImg = img(plates[i]);
+
+            imwrite("Resources/plate" + to_string(i + 1) + ".png", plateImg);
+            rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
+
+            extractCharacters(plateImg, "nummerbord" + to_string(i + 1));
+
+        }
+
+        imshow("Image", img);
+
+        if (waitKey(1) == 27)
+            break;
+    }
+
+    return 0;
+}
